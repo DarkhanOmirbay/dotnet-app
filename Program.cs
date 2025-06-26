f28a9f42d90f45aa1822251b79321d93cdf75105
DarkhanOmirbay <domirbai04@gmail.com>
2025-06-26 23:17:16 +0500

    add .env load and cors configuration

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
 using MyApi.Interfaces;
 using MyApi.Services;
 using Microsoft.OpenApi.Models;
+using DotNetEnv;
+
+// .env
+DotNetEnv.Env.Load();
+string? originsEnv = Environment.GetEnvironmentVariable("ORIGINS");
+string[] allowedOrigins = originsEnv?.Split(",") ?? Array.Empty<string>();
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,7 +20,26 @@ builder.Services.AddSwaggerGen(c =>
 builder.Services.AddSingleton<ITodoService, TodoService>();
 
 // lowercase urls
-builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true); 
+builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
+// add cors
+builder.Services.AddCors(
+    options =>
+    {
+        options.AddDefaultPolicy(
+            policy =>
+            {
+                policy.WithOrigins(allowedOrigins).AllowAnyHeader()
+                  .AllowAnyMethod();
+            }
+        );
+    }
+);
+
+Console.WriteLine("ALLOWED ORIGINS:");
+foreach (var o in allowedOrigins)
+{
+    Console.WriteLine(o);
+}
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
@@ -22,6 +47,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+// any origins urls
+app.UseCors();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
